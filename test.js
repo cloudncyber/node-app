4b4b684be8336d6f0d47cb7d83eb2d06de856141
cloudncyber <104633651+cloudncyber@users.noreply.github.com>
2024-08-31 21:20:55 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Task API', () => {
+  it('should add a task', () => {
+    // Mock test implementation
+    expect(true).to.be.true;
+  });
+});
